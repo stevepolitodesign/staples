1091f305e1c6de99908d17f253743baca5b96a98
Steve Polito <stevepolitodesign@users.noreply.github.com>
2025-11-26 21:20:30 -0500

    Prepare for release (#6)
    Since each release is a breaking change, we bump to `1.0.0`.

--- lib/staples/version.rb
+++ lib/staples/version.rb
@@ -3,5 +3,5 @@
 # The Staples module provides a CLI tool for generating opinionated Rails applications.
 module Staples
   # The current version of the Staples gem.
-  VERSION = "0.1.0"
+  VERSION = "1.0.0"
 end
